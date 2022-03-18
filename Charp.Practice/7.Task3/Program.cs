f7db7f3f12b187570ea699002720fc83e7353eb6
Lesay0786 <Lesay_kl@mail.ru>
2022-03-18 21:54:04 +0300

    done 2 tasks

---
+++ Charp.Practice/7.Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] array = new int[13];
+Random rnd = new Random();
+int a = int.MinValue;
+for (int i = 0; i < array.Length; i++)
+{
+    int number = rnd.Next(0, 30);
+    array[i] = number;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write($"{array[i]} ");
+
+}
+Console.WriteLine();
+Console.WriteLine();
+
+for (int i = 0; i < array.Length; i++)
+{
+ if (array[i] > a)
+    {
+        a=array[i];
+    }
+}
+Console.Write($"Max = {a} ");
