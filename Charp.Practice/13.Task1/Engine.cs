2752758ba99f29a62be10e5e9d6c54b847587306
Алеся Максимова <lesay_kl@mail.ru>
2022-04-06 19:59:42 +0300

    Bad

---
+++ Charp.Practice/13.Task1/Engine.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Task1
+{
+    public class Engine
+    {
+        public int Volume
+        {
+            get
+            {
+                return Volume;
+            }
+            set
+            {
+                if (value <= 125 && value >= 1300)
+                {
+                    Volume = value;
+                }
+                else
+                {
+                    Console.WriteLine("Объем не должен превышать от 125 до 1300 куб. м");
+                }
+            }
+        }
+        public int Capacity
+        {
+            get
+            {
+                return Capacity;
+            }
+            set
+            {
+                if (value <= 50 || value >= 400)
+                {
+                    Capacity = value;
+                }
+
+                else
+                {
+                    Console.WriteLine("Мощность не должна превышать 50 до 400 лошб сил");
+                }
+            }
+
+        }
+
+    }
+}
---
+++ Charp.Practice/13.Task1/Motorycle.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Task1
+{
+    internal class Motorycle
+    {
+        public int Id
+        {
+            get
+            {
+                return Id;
+            }
+            private set
+            {
+                Id = value;
+            }
+        }
+        public string? Model { get; set; }
+        public string? Manufacturer { get; set; }
+       
+        public DateTime YearOfIssue
+        {
+            get
+            {
+                return YearOfIssue;
+            } 
+            set
+            {
+                YearOfIssue = DateTime.Now;
+            }
+        }
+        public int Mileage
+        {
+            get
+            {
+                return Mileage;
+            }
+            set
+            {
+                if (Mileage <= 100)
+                {
+                    Mileage = value;
+                }
+                else
+                {
+                    Console.WriteLine("Пробег не должен превышать 100");
+                }
+            }
+        }
+
+        public Motorycle(string model, string mo)
+        {
+
+        }
+        protected void Reset()
+        {
+            Mileage = default;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+}
